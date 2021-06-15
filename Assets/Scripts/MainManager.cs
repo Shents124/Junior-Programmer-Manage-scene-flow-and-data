4d0c49c424e7da6b1bfdb7362d3b361ded0215bd
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-06-15 09:02:17 +0700

    Add method LoadColor to get color is store

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -17,6 +17,9 @@ public class MainManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        
+        // Load the color is stored
+        LoadColor();
     }
     
     [System.Serializable]
@@ -35,6 +38,21 @@ public class MainManager : MonoBehaviour
         string json = JsonUtility.ToJson(data);
         // Create file savefile.json to store text json 
         File.WriteAllText(Application.persistentDataPath + "/savefile.json",json);
+    }
+
+    public void LoadColor()
+    {
+        // get file stored data
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            // Read all text in path and assign to json variable
+            string json = File.ReadAllText(path);
+            // Transform json data to SaveData
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            // Set teamColor in menu to the color saved in SaveData
+            teamColor = data.teamColor;
+        }
     }
 }
 
