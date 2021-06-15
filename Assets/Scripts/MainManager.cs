de2ebde14ce28cff479ba7bba93918ec2ca7fb69
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-06-15 07:47:29 +0700

    Create MainManager using Singleton

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // Start() and Update() methods deleted - we don't need them right now
+    public static MainManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
